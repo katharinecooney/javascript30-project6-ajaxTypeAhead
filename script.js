c8402ae0b891306ba9fab2d011cb34e1d14281e0
Katharine Cooney <katharinecooney@gmail.com>
2019-06-14 21:24:18 +0200

    Add comments to the js file; add function to add commas to the population values

--- script.js
+++ script.js
@@ -2,38 +2,43 @@ const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb
 
 const cities = [];
 
-// fetch returns a promise
+// fetch will pull the data from the endpoint that we pass
+// fetch will not return the data - it will return a promise!
+
 fetch(endpoint)
   .then(blob => blob.json())
   .then(data => cities.push(...data))
-
-function displayMatches() {
-  const matchArray = findMatches(this.value, cities);
-  const html = matchArray.map(place => {
-    const regex = new RegExp(this.value, 'gi');
-    const cityName = place.city.replace(regex, `<span class='highlight'>${this.value}</span>`);
-    const stateName = place.state.replace(regex, `<span class='highlight'>${this.value}</span>`);
-    return `
-    <li>
-      <span class='name'>${cityName}, ${stateName}</span>
-      <span class='population'>${place.population}</span>
-    </li>
-    `;
-  }).join('');
-  suggestions.innerHTML = html;
-}
-
+  
 findMatches = (wordToMatch, cities) => {
   return cities.filter(place => {
-  const regex = new RegExp(wordToMatch, 'gi');
-  return place.city.match(regex) || place.state.match(regex)
+    const regex = new RegExp(wordToMatch, 'gi');
+    return place.city.match(regex) || place.state.match(regex)
   })
 }
 
 const searchInput = document.querySelector('.search');
-const suggestions = document.querySelector('.suggestions');
+const suggestions = document.querySelector('.suggestions')
 
-searchInput.addEventListener('change', displayMatches)
-searchInput.addEventListener('keyup', displayMatches)
+// this function was pulled from StackOverflow; if will add commas to our populations
+function numberWithCommas(x) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
 
+// this function will be called anytime someone changes the value in the search bar
+function displayMatches() {
+  const foundMatches = findMatches(this.value, cities);
+  const html = foundMatches.map(place => {
+    const regex = new RegExp(this.value, 'gi');
+    const cityName = place.city.replace(regex, `<span class="highlight">${this.value}</span>`);
+    const stateName = place.state.replace(regex, `<span class="highlight">${this.value}</span>`);
+    return `<li>
+      <span>${cityName}, ${stateName}</span>
+      <span>${numberWithCommas(place.population)}</span>
+    </li>`
+  }).join('');
+  suggestions.innerHTML = html;
+}
 
+// the change event is triggered only when we click out of the search bar after typing; for this reason, we also add an event listener on keyup
+searchInput.addEventListener('change', displayMatches);
+searchInput.addEventListener('keyup', displayMatches);
